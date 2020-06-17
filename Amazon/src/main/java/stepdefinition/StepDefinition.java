962ad96cc39101d9c8e05e6733a45dca0abc9388
Monsur26 <monsur.ahmed26@gmail.com>
2020-06-17 13:18:44 -0400

    first commit

---
+++ Amazon/src/main/java/stepdefinition/StepDefinition.java
@@ -0,0 +1,23 @@
+package stepdefinition;
+
+import common.WebAPI;
+import home.Homepage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+
+public class StepDefinition extends WebAPI {
+    @Before
+    public void openBrowser() throws IOException {
+        setUp(false, "browserstack", "windows", "10", "chrome", "83", "https://www.amazon.com/");
+    }
+    @After
+    public void closeBrowser() {
+        cleanUp();
+    }
+    static Homepage homepage;
+    public static void getInitElements(){homepage=PageFactory.initElements(driver,Homepage.class);}
+
+}
